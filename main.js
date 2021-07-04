46722b4706553acd2b9b5f8624d93a29f60381ab
ruveer <81567814+ruveer@users.noreply.github.com>
2021-07-04 13:00:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+menu_list_array = ["Veg Margherita Pizza",//add more items
+                    ];
+
+function getmenu(){
+var htmldata;
+//Complete the code
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+for(var i=0;i<menu_list_array.length;i++){
+    html=htmldata+'<li>' + menu_list_array[i]
+}
+}
+
+function add_top(){
+//Complete the code
+}
